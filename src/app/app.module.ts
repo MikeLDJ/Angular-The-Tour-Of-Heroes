c469e823eb8a885d17843bf438526b9232a8d9c7
MikeLDJ <mikeu76@gmail.com>
2017-06-13 16:03:19 +0200

    added server in memory service, http module, change, add and delete heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroService } from './hero.service';
+import { AppComponent } from './app.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'
+import { HeroesComponent } from './heroes/heroes.component'
+import { HeroService } from './hero.service'
 import { DashboardComponent } from './dashboard/dashboard.component'
 
 import { AppRoutingModule } from './app-routing/app-routing.module'
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api'
+import { InMemoryDataService } from './in-memory-data.service'
 
 @NgModule({
   declarations: [
@@ -20,7 +24,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module'
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
